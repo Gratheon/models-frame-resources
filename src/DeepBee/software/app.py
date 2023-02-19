3581bf6647f5ffbc56f656c8becacd5c151eb000
Artjom Kurapov <artjom.kurapov@clarifai.com>
2023-02-20 01:06:44 +0200

    added test image

--- src/DeepBee/software/app.py
+++ src/DeepBee/software/app.py
@@ -1,24 +1,7 @@
-# from flask import Flask
 from detection_and_classification import run
-# from platform import python_version
-
-# app = Flask(__name__)
-
-# print("Python version: ", python_version())
-
-# @app.route("/")
-# def hello():
-#     # try:
-#     #     print("running")
-#     run()
-#     #     print("Done")
-#     # except Exception as e:
-#     #     print(e)
-#     return "Hello, World!"
 
 if __name__ == "__main__":
     run()
-    # app.run(host="0.0.0.0", port=8540, debug=True, threaded=False)
 
 
 # import asyncio
